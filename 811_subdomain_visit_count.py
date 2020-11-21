e88090a3d910f3c411e1f6175b1f66488b1d7fa3
Jane Yang <50490168+SavannahY@users.noreply.github.com>
2020-11-20 20:08:54 -0800

    Create 811_subdomain_visit_count.py

---
+++ 811_subdomain_visit_count.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=811 lang=python3
+#
+# [811] Subdomain Visit Count
+#
+
+# @lc code=start
+class Solution:
+    def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
+        count = collections.Counter()
+        for cp in cpdomains:
+            n, s = cp.split()
+            count[s] += int(n)
+            for i in range(len(s)):
+                if s[i] == '.':
+                    count[s[i+1:]] += int(n)
+        return ["%d %s" % (count[k],k) for k in count]
